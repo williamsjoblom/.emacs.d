123a29201060bfc2c6a2d0fc05656a4c5e25e655
williamsjoblom <wllmsjblm@gmail.com>
2019-02-05 14:19:04 +0100

    Add automatic prettification of symbols

--- settings/org-init.el
+++ settings/org-init.el
@@ -1,4 +1,8 @@
+;; Syntax highlighting for code blocks.
 (setq org-src-fontify-natively t)
 (setq org-src-tab-acts-natively t)
+
+;; Prettify symbols.
+(setq org-pretty-entities t)
 
 (provide 'org-init)
