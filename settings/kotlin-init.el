70a88fa49210d2680a17776836f0e600e282450f
William Sjöblom <wllmsjblm@gmail.com>
2019-03-05 12:15:43 +0100

    Fix multiline statement indentation for kotlin

--- settings/kotlin-init.el
+++ settings/kotlin-init.el
@@ -2,24 +2,42 @@
    (make-lsp-client :new-connection (lsp-stdio-connection
                                      "/opt/kotlin-language-server/bin/kotlin-language-server")
                     :major-modes '(kotlin-mode)
-                    :priority -1
+                    :priority -2
                     :ignore-messages '("readFile .*? requested by Kotlin but content not available")
                     :server-id 'kotlin-ls))
 
-(defun my/kotlin-mode-hook ()
+;; kotlin-mode has severe indentation issues. Since swift is pretty similar indentation wise
+;; we use its indentation rules instead.
+(use-package swift-mode
+  :ensure t)
+
+(defun my/kotlin-mode-hook ()  
   (lsp)
   (lsp-mode)
   (company-mode)
-  (flycheck-mode))
+  (flycheck-mode)
+  (gradle-mode)
+
+  ;; Use swift indentation rules.
+  (setq-local swift-mode:parenthesized-expression-offset 8)
+  (setq-local swift-mode:multiline-statement-offset 8)
+  (setq-local indent-line-function #'swift-mode:indent-line))
+
+(use-package gradle-mode
+  :ensure t)
+
+(use-package groovy-mode
+  :ensure t
+  :mode "\\.gradle\\'")
+
 
 (use-package kotlin-mode
   :ensure t
   :mode "\\.kt\\'"
-  :after company-lsp
+  :after (company-lsp gradle-mode swift-mode)
   :hook (kotlin-mode . my/kotlin-mode-hook)
   :config
-  (setq-default kotlin-tab-width 4)
-  (setq-default indent-tabs-mode nil))
+  )
 
 
 (provide 'kotlin-init)
