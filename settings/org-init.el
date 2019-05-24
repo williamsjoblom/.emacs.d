7132bff604e4e069c3589b0f422034a9600e6112
William Sjöblom <wllmsjblm@gmail.com>
2019-05-24 14:02:21 +0200

    Move org underline emph to hook

--- settings/org-init.el
+++ settings/org-init.el
@@ -5,7 +5,10 @@
 ;; Prettify symbols.
 (setq org-pretty-entities t)
 
-(add-to-list 'org-emphasis-alist
-             '("_" (:foreground "red")))
+(defun my/org-mode-hook ()
+  (add-to-list 'org-emphasis-alist
+             '("_" (:foreground "red"))))
+
+(add-hook 'org-mode-hook)
 
 (provide 'org-init)
