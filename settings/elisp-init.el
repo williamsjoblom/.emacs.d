6e11ba2d2c2a26e87b775d9c826d582bffa29d50
williamsjoblom <wllmsjblm@gmail.com>
2019-05-26 17:44:42 +0200

    Add function for inserting elisp block comments

--- settings/elisp-init.el
+++ settings/elisp-init.el
@@ -1,5 +1,22 @@
+(defun elisp-block-comment ()
+  "Insert elisp block comment above current line."
+  (interactive)
+  (beginning-of-line)
+  (open-line 1)
+  (indent-according-to-mode)
+  (let* ((indent (current-column))
+	 (dashes (make-string (- 80 indent) ?-)))
+    (insert ";;" dashes "\n")
+    (indent-to indent)
+    (insert ";; \n")
+    (indent-to indent)
+    (insert ";;" dashes)
+    (end-of-line 0)))
+
+
 (defun my/elisp-hook ()
-  (company-mode))
+  (company-mode)
+  (local-set-key (kbd "C-M-j") 'elisp-block-comment))
 
 (add-hook 'emacs-lisp-mode-hook 'my/elisp-hook)
 
