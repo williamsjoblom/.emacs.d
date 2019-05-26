7e656548fe6ff230480b888a6bbf700104e33013
williamsjoblom <wllmsjblm@gmail.com>
2019-05-26 17:43:13 +0200

    Bind <f*> to <f*> to jump and set bookmarks respectively

--- settings/key-bindings.el
+++ settings/key-bindings.el
@@ -34,4 +34,32 @@
 
 (global-set-key (kbd "C-]") 'comment-or-uncomment-region)
 
+;; Bookmark bindings
+(global-set-key (kbd "<f1>") (lambda () (interactive) (bookmark-jump "f1")))
+(global-set-key (kbd "<f2>") (lambda () (interactive) (bookmark-jump "f2")))
+(global-set-key (kbd "<f3>") (lambda () (interactive) (bookmark-jump "f3")))
+(global-set-key (kbd "<f4>") (lambda () (interactive) (bookmark-jump "f4")))
+(global-set-key (kbd "<f5>") (lambda () (interactive) (bookmark-jump "f5")))
+(global-set-key (kbd "<f6>") (lambda () (interactive) (bookmark-jump "f6")))
+(global-set-key (kbd "<f7>") (lambda () (interactive) (bookmark-jump "f7")))
+(global-set-key (kbd "<f8>") (lambda () (interactive) (bookmark-jump "f8")))
+(global-set-key (kbd "<f9>") (lambda () (interactive) (bookmark-jump "f9")))
+(global-set-key (kbd "<f10>") (lambda () (interactive) (bookmark-jump "f10")))
+(global-set-key (kbd "<f11>") (lambda () (interactive) (bookmark-jump "f11")))
+(global-set-key (kbd "<f12>") (lambda () (interactive) (bookmark-jump "f12")))
+
+
+(global-set-key (kbd "C-<f1>") (lambda () (interactive) (bookmark-set "f1")))
+(global-set-key (kbd "C-<f2>") (lambda () (interactive) (bookmark-set "f2")))
+(global-set-key (kbd "C-<f3>") (lambda () (interactive) (bookmark-set "f3")))
+(global-set-key (kbd "C-<f4>") (lambda () (interactive) (bookmark-set "f4")))
+(global-set-key (kbd "C-<f5>") (lambda () (interactive) (bookmark-set "f5")))
+(global-set-key (kbd "C-<f6>") (lambda () (interactive) (bookmark-set "f6")))
+(global-set-key (kbd "C-<f7>") (lambda () (interactive) (bookmark-set "f7")))
+(global-set-key (kbd "C-<f8>") (lambda () (interactive) (bookmark-set "f8")))
+(global-set-key (kbd "C-<f9>") (lambda () (interactive) (bookmark-set "f9")))
+(global-set-key (kbd "C-<f10>") (lambda () (interactive) (bookmark-set "f10")))
+(global-set-key (kbd "C-<f11>") (lambda () (interactive) (bookmark-set "f11")))
+(global-set-key (kbd "C-<f12>") (lambda () (interactive) (bookmark-set "f12")))
+
 (provide 'key-bindings)
