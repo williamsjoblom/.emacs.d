60940438e501c3cc28a87483e65e39b5c748bbd0
williamsjoblom <wllmsjblm@gmail.com>
2019-07-11 14:31:14 +0200

    Add better TODOs

--- settings/org-init.el
+++ settings/org-init.el
@@ -7,7 +7,18 @@
 
 (defun my/org-mode-hook ()
   (add-to-list 'org-emphasis-alist
-             '("_" (:foreground "red"))))
+               '("_" (:foreground "red"))))
+
+(setq org-todo-keywords
+      '((sequence "TODO" "|" "VERIFY" "DONE")))
+(setq org-log-done 'time)
+
+(defun org-summary-todo (n-done n-not-done)
+  "Switch entry to DONE when all subentries are done, to TODO otherwise."
+  (let (org-log-done org-log-states)   ; turn off logging
+    (org-todo (if (= n-not-done 0) "DONE" "VERIFY" "TODO"))))
+
+(add-hook 'org-after-todo-statistics-hook 'org-summary-todo)
 
 (add-hook 'org-mode-hook 'my/org-mode-hook)
 
