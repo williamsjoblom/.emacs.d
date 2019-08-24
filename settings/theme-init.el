a12557e98e954a8bb4e7d53529fed13afab77487
William Sjöblom <wllmsjblm@gmail.com>
2019-08-24 09:48:50 +0200

    Add theme-init

---
+++ settings/theme-init.el
@@ -0,0 +1,26 @@
+(use-package gruvbox-theme
+  :ensure t
+  :config
+  (load-theme 'gruvbox-light-hard t)
+  (let ((line (face-attribute 'mode-line :underline)))
+    (set-face-attribute 'mode-line          nil :overline   line)
+    (set-face-attribute 'mode-line-inactive nil :overline   line)
+    (set-face-attribute 'mode-line-inactive nil :underline  line)
+    (set-face-attribute 'mode-line          nil :box        nil)
+    (set-face-attribute 'mode-line-inactive nil :box        nil)
+    (set-face-attribute 'mode-line          nil :background "#f2e5bc")
+    (set-face-attribute 'mode-line-inactive nil :background "#ebdbb2")))
+
+(use-package moody
+  :ensure t
+  :config
+  (setq x-underline-at-descent-line t)
+  (moody-replace-mode-line-buffer-identification)
+  (moody-replace-vc-mode))
+
+(use-package minions
+  :ensure t
+  :config
+  (minions-mode 1))
+
+(provide 'theme-init)
