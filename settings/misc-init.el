099bf1e13cd4066735aad3eac79a0b292f740b0a
williamsjoblom <wllmsjblm@gmail.com>
2019-11-17 16:10:34 +0100

    Fix unbalanced parentheses

--- settings/misc-init.el
+++ settings/misc-init.el
@@ -14,7 +14,7 @@
 
   (setq dashboard-set-heading-icons t)
   (setq dashboard-set-file-icons t)
-  (setq dashboard-center-content t))
+  (setq dashboard-center-content t)
   (setq dashboard-items '((recents . 10)
                           (projects . 3)
                           (bookmarks . 5)
