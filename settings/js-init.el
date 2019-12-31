e54ef362f3cfdcfeb9a035e971ac722da9fbb864
William Sjöblom <wllmsjblm@gmail.com>
2019-12-31 12:25:51 +0100

    Add tern

--- settings/js-init.el
+++ settings/js-init.el
@@ -1,8 +1,27 @@
+(use-package company-tern
+    :ensure t
+    :config
+    (add-to-list 'company-backends 'company-tern))
+
+(use-package tern
+  :ensure t
+  :commands tern-mode
+  :after company-tern
+  :hook (tern-mode . company-mode)
+  :config
+  (define-key tern-mode-keymap (kbd "C-M-.") nil)
+
+  (define-key tern-mode-keymap (kbd "M-i") 'tern-find-definition)
+  (define-key tern-mode-keymap (kbd "C-M-i") 'tern-pop-find-definition))
+
+
+
 (use-package js2-mode
   :ensure t
   :mode
   ("\\.js\\'" . js2-mode)
   ("\\.jsx\\'" . js2-jsx-mode)
+  :hook (js2-mode . tern-mode)
   :bind (:map global-map
 	      ("C-M-k" . c-doc-comment)
 	      ("C-M-j" . c-block-comment)))
