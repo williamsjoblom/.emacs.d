f14fe7a27534287862f7bca147579cdeb25bae92
William Sjöblom <wllmsjblm@gmail.com>
2020-02-25 19:01:29 +0100

    Add new fancy theme

--- settings/theme-init.el
+++ settings/theme-init.el
@@ -1,15 +1,21 @@
-(use-package gruvbox-theme
+;; (use-package gruvbox-theme
+;;   :ensure t
+;;   :config
+;;   (load-theme 'gruvbox-light-hard t)
+;;   (let ((line (face-attribute 'mode-line :underline)))
+;;     (set-face-attribute 'mode-line          nil :overline   line)
+;;     (set-face-attribute 'mode-line-inactive nil :overline   line)
+;;     (set-face-attribute 'mode-line-inactive nil :underline  line)
+;;     (set-face-attribute 'mode-line          nil :box        nil)
+;;     (set-face-attribute 'mode-line-inactive nil :box        nil)
+;;     (set-face-attribute 'mode-line          nil :background "#f2e5bc")
+;;     (set-face-attribute 'mode-line-inactive nil :background "#ebdbb2")))
+
+(use-package dakrone-light-theme
   :ensure t
   :config
-  (load-theme 'gruvbox-light-hard t)
-  (let ((line (face-attribute 'mode-line :underline)))
-    (set-face-attribute 'mode-line          nil :overline   line)
-    (set-face-attribute 'mode-line-inactive nil :overline   line)
-    (set-face-attribute 'mode-line-inactive nil :underline  line)
-    (set-face-attribute 'mode-line          nil :box        nil)
-    (set-face-attribute 'mode-line-inactive nil :box        nil)
-    (set-face-attribute 'mode-line          nil :background "#f2e5bc")
-    (set-face-attribute 'mode-line-inactive nil :background "#ebdbb2")))
+  (load-theme 'dakrone-light t))
+
 
 (use-package moody
   :ensure t
