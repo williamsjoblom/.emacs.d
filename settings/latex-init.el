a86d0aadd992fa16efa1c85bbc6be913b41176c5
William Sjöblom <wllmsjblm@gmail.com>
2021-01-27 18:22:28 +0100

    Add flyspell for latex

--- settings/latex-init.el
+++ settings/latex-init.el
@@ -1,6 +1,11 @@
 (use-package auctex
     :ensure t
     :hook (LaTeX-mode . hl-todo-mode)
-    :hook (LaTex-mode . flyspell-mode))
+    :hook (LaTeX-mode . flyspell-mode))
+
+(eval-after-load "flyspell" '(define-key flyspell-mode-map (kbd "C-.") nil))
+(eval-after-load "flyspell" '(define-key flyspell-mode-map (kbd "C-,") nil))
+
+
 
 (provide 'latex-init)
