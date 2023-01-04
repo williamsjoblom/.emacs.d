5310fd368f60ab6a5144cb44c677d7a0af4747fb
William Sjöblom <williams@opera.com>
2023-01-04 14:17:55 +0100

    Add TypeScript configuration

--- settings/js-init.el
+++ settings/js-init.el
@@ -1,12 +1,40 @@
+(defun setup-work-node-path ()
+  (let ((node-path (expand-file-name "~/.nvm/versions/node/v14.21.1/bin/")))
+    (unless (member node-path exec-path)
+      (setq exec-path (append exec-path `(,node-path)))
+      (setenv "PATH" (concat (getenv "PATH") ":" node-path)))))
+
 (use-package tide
   :ensure t
   :bind (:map global-map
 	      ("C-M-k" . c-doc-comment)
 	      ("C-M-j" . c-block-comment)))
 
-(defun setup-tide ()
+(defun my/ts-hook ()
+  (interactive)
+
+  (when (at-work-p)
+    (setup-work-node-path))
+
+  (lsp-mode)
+  (flycheck-mode 1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+
+  (eldoc-mode 1)
+  (company-mode 1))
+
+(use-package typescript-mode
+  :ensure t
+  :mode ("\\.ts\\'" . typescript-mode)
+  :hook (typescript-mode . my/ts-hook))
+
+(defun my/js-hook ()
   "Setup tide-mode and friends for current buffer"
   (interactive)
+
+  (when (at-work-p)
+      (setup-work-node-path))
+
   (tide-setup)
 
   (flycheck-mode 1)
@@ -16,7 +44,7 @@
   (tide-hl-identifier-mode 1)
   (company-mode 1))
 
-(add-hook 'js-mode-hook #'setup-tide)
+(add-hook 'js-mode-hook #'my/js-hook)
 
 ;; (use-package js2-mode
 ;;   :ensure t
